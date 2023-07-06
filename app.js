56fe670eec66df75d0290005cc20c11491e5fc33
Matthew Barhou <barhoumatthew@gmail.com>
2023-07-06 17:40:42 -0400

    Added Hash Authentication

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -19,8 +18,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
-
 const User = new mongoose.model("User", userSchema);
 
 
@@ -28,48 +25,45 @@ app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get("/login", function (req, res) {
-    res.render("login");
-});
-
 app.get("/register", function (req, res) {
     res.render("register");
-});
-
-app.post("/login", function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email: username})
-    .then((foundUser) => {
-        if (foundUser) {
-            if (foundUser.password === password) {
-                res.render("secrets");
-            }
-        }
-    })
-    .catch((err) => {
-        console.log(err);
-    });
 });
 
 app.post("/register", function (req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save()
-    .then(() => {
-        res.render("secrets");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+        .then(() => {
+            res.render("secrets");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
+app.get("/login", function (req, res) {
+    res.render("login");
+});
 
+app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = md5(req.body.password);
 
+    User.findOne({ email: username })
+        .then((foundUser) => {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                }
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
 
 
